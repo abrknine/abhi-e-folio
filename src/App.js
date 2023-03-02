437f3bba26c34878ff8382b4a3f718f9332b6399
abrknine <abhishekbahukhandi2020@gmail.com>
2023-03-03 01:16:35 +0530

    added r-navbar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./componenets/Navbar";
+import Home from "./componenets/Home";
+
+function App() {
+  return (
+    <div >
+     <Navbar/>
+     <Home/>
+    </div>
+  );
+}
+
+export default App;
