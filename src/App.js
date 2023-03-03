dfb84427208200319e1a752de19729ae5c38a8c5
abrknine <abhishekbahukhandi2020@gmail.com>
2023-03-03 14:19:54 +0530

    added home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Navbar from "./componenets/Navbar";
 import Home from "./componenets/Home";
+import SocialLinks from "./componenets/SocialLinks";
 
 function App() {
   return (
     <div >
      <Navbar/>
      <Home/>
+     
+     <SocialLinks/>
     </div>
   );
 }
